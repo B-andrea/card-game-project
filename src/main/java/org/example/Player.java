e6fe868821ae5c0bc0a6dedd18508733eb3864d8
BCruzMejia <BCruzMejia@Bs-MacBook-Pro.communityfibre.co.uk>
2025-02-16 19:15:33 +0000

    Added method to alternate players

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,3 +1,5 @@
+package org.example;
+
 // Keep track of players and score
 
 public class Player {
--- src/main/java/org/example/SnapGame.java
+++ src/main/java/org/example/SnapGame.java
@@ -1,13 +1,18 @@
 package org.example;
-
 import java.util.Scanner;
 
 public class SnapGame extends CardGame {
-    Boolean isGameOver = false;
-    Card previousCard;
+    Boolean isGameOver = false; // track whether the game is finished
+    Card previousCard; // store the previous card that was dealt, so that the next card can be compared to it
+    Player player1; // enabling two players
+    Player player2;
+    Player currentPlayer; // keep track of which player is taking the current turn
 
     public SnapGame() {
         super("Snap");
+        player1 = new Player("Player 1");
+        player2 = new Player("Player 2");
+        currentPlayer = player1; // Player 1 starts - initialising currentPlayer variable to player1
     }
 
     public void playGame() {
@@ -33,5 +38,16 @@ public class SnapGame extends CardGame {
 
             previousCard = nextCard;
         }
+
+        // Users take turns
+        public void switchUser() {
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
+            } else {
+                currentPlayer = player1;
+            }
+        }
+
+
     }
 }
