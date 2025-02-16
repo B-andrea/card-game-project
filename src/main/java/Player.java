91662a712ca3ff8659c869c02c2ba0baa2905546
BCruzMejia <BCruzMejia@Bs-MacBook-Pro.communityfibre.co.uk>
2025-02-16 18:00:13 +0000

    Added getter methods for names and scores

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -10,14 +10,11 @@ public class Player {
         this.score = 0;
     }
 
+    public String getName() {
+        return name;
+    }
 
-
-
-
-
-
-
-
-
-
+    public int getScore() {
+        return score;
+    }
 }
